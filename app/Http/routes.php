0e160438f4cb84e990c3f558987c476c9ce62e35
Emilio Ochoa <emilioaor@gmail.com>
2017-04-16 02:10:00 -0400

    Nuevo inicio del proyecto

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,32 +11,5 @@
 |
 */
 
-//Rutas Index
-Route::get('/','indexController@index');
-Route::get('/{id}/video/{video}','indexController@video');
-Route::get('/{slug}','indexController@movie');
-Route::post('/subscriber/add','indexController@suscription');
-Route::get('/download/{id}','indexController@download');
-Route::get('/subscriber/agradecimientos','indexController@graxx');
-Route::get('/error/404','indexController@error404');
-// Alertas
-Route::get('alert/success','indexController@success');
-Route::get('alert/fail','indexController@fail');
 
-//Rutas Generos
-Route::get('genero/{genero}','genreController@index');
-
-// Rutas de Autorizacion
-Route::get('auth/login','authController@login')->middleware('checkAuth');
-Route::post('auth/authenticate','authController@authenticate')->middleware('checkAuth');
-Route::get('auth/logout','authController@logoutuser');
-
-// Rutas Administrador
-Route::group(['prefix'	=> 'admin','middleware' => 'admin'],function(){
-	Route::resource('movies','moviesController');
-	Route::get('fast/update','moviesController@fast');
-	Route::resource('genres','adminGenreController');
-	Route::resource('relations','relationsController');
-	Route::get('subscribers','subscribersController@index');
-});
 
